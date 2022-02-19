5289853c57036a5c828d283f85a196197c478f77
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2022-02-19 12:39:32 +0600

    Matplotlib : Squares - Values

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-squares = [x**2 for x in range(1,5)]
+value = range(1, 6)
+squares = [x**2 for x in value]
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=1)
+ax.plot(value, squares, linewidth=1)
 
 # Назначение заголовка и меток осей
 ax.set_title("Квадраты", fontsize=10)
