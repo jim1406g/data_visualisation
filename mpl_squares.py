6551e79c06689a6a874bcb71d35dc778930e6cdf
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2022-02-19 11:38:04 +0600

    Matplotlib : Squares - Labels

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,6 +2,13 @@ import matplotlib.pyplot as plt
 
 squares = [x**2 for x in range(1,5)]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=1)
+
+# Назначение заголовка и меток осей
+ax.set_title("Квадраты", fontsize=10)
+ax.set_xlabel("Значения", fontsize=10)
+ax.set_ylabel("Квадраты значений", fontsize=10)
+
+ax.tick_params(axis='both', labelsize=10)
 
 plt.show()
